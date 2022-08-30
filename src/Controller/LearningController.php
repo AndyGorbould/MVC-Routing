81c394b9b301dfb02239e5d4d7ea2704b9976ff2
Andy Gorbould <gorbould@mail.com>
2022-08-30 16:51:03 +0200

    path changed to about-me

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -8,8 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController
 {
-    #[Route('/learning', name: 'app_learning')]
-    public function aboutMe(): Response
+    #[Route('/about-me', name: 'app_learning')] // 👈👈👈 '/about-me' is the path
+    public function aboutMe(): Response // 🤏 in 'routes.yaml' 'controller: App\Controller\LearningController::aboutMe' the '::aboutMe' refers to the action() being called
     {
         $aboutMe = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut hendrerit urna, quis laoreet neque. Donec semper justo vitae libero aliquam accumsan. Proin turpis orci, semper et tortor non, pretium congue ligula. Integer eu orci purus. Aenean porta nisi quam, id tempus nunc sollicitudin non. Maecenas ut laoreet eros. Aliquam.';
         return $this->render('learning/index.html.twig', [
